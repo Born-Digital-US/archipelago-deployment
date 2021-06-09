a076313a614d408d3f684acf00e37a5817e36d1a
Pat Dunlavey <pat@born-digital.com>
2021-06-09 15:00:42 -0400

    CAR-config-merge2 --- tweaks to cls_migrate bash script

--- web/modules/custom/cls_migrate/scripts/migrate_cls.sh
+++ web/modules/custom/cls_migrate/scripts/migrate_cls.sh
@@ -3,12 +3,17 @@
 # run this from the root directory so that the files rsync will happen in the right place. E.g. `sh web/modules/custom/cls_migrate/scripts/migrate_cls.sh`
 
 # rsync the files directory to /web/migrate_files
-rsync -rltv --exclude=js  --exclude=css --exclude=backup_migrate/* --exclude=feeds/* islandora@repository.californialightandsound.org:/opt/data/apache_cls/html/sites/default/files/ web/migrate_files/
-docker exec -it esmero-php bash -c "drush config-split:import car -y"
+rsync -rltv --exclude=js  --exclude=ctools --exclude=css --exclude=private --exclude=backup_migrate/* --exclude=feeds/* islandora@repository.californialightandsound.org:/opt/data/apache_cls/html/sites/default/files/ web/migrate_files/
+#docker exec -it esmero-php bash -c "drush config-split:import car -y"
+
+#Reinstall cls_migrate to get the latest configs.
+docker exec -it esmero-php bash -c "drush dre cls_migrate"
 
 docker exec -it esmero-php bash -c "drush mim cls_file"
 docker exec -it esmero-php bash -c "drush mim cls_user_role"
-# docker exec -it esmero-php bash -c "drush mim cls_taxonomy_vocabularies"
+# Even though the vocabs exist. We run this to populate the migrate map with the vocabulary identifiers
+# for subsequent taxonomy imports to look up.
+docker exec -it esmero-php bash -c "drush mim cls_taxonomy_vocabularies"
 docker exec -it esmero-php bash -c "drush mim cls_taxonomy_term_vendor_services"
 docker exec -it esmero-php bash -c "drush mim cls_taxonomy_term_production_stream"
 docker exec -it esmero-php bash -c "drush mim cls_taxonomy_term_lto_generations"
